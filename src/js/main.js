a8d72b3de97c28044e52148051561db23a4ae563
EstLopCor <estherlopezcordoncillo@gmail.com>
2021-08-02 12:02:47 +0200

    add arrow to modal container

--- src/js/main.js
+++ src/js/main.js
@@ -9,25 +9,9 @@ if (document.getElementById("btnModal")) {
   btn.onclick = function () {
     modal.style.display = "block";
     body.style.position = "static";
-    body.style.height = "100%";
-    body.style.overflow = "hidden";
   };
 
   span.onclick = function () {
     modal.style.display = "none";
-
-    body.style.position = "inherit";
-    body.style.height = "auto";
-    body.style.overflow = "visible";
-  };
-
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-
-      body.style.position = "inherit";
-      body.style.height = "auto";
-      body.style.overflow = "visible";
-    }
   };
 }
